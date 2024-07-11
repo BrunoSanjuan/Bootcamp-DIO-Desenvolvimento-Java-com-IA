d317f31081a5e096b2a8ea154c5a41f281a06642
Bruno Rodrigues Alves Sanjuan <bruno_sanjuan@yahoo.com>
2024-07-11 15:47:54 -0300

    #

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -5,6 +5,6 @@ public class TiposVariaveis {
         short ano = 2024;
         int cep = 123456789; // Se começar com valor 0 talvez seja necessário usar outro tipo.
         long cpf = 19876543210L; // long precisa terminar com L; Se começar com valor 0 talvez seja necessário usar outro tipo.
-        float PI = 3.14F; // float precisa terminar com F
+        float PI = 3.14F; // float precisa terminar com F; variável em MAIÚSCULO ou a palavra "final" antes de qualquer variável lhe tornará uma variável de valor constante.
     }
 }
