6d27ec844ce0a324426b064cf277b3387b2bdac1
Bruno Rodrigues Alves Sanjuan <bruno_sanjuan@yahoo.com>
2024-07-11 15:45:28 -0300

    #

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 1412.00;
+        byte idade = 27;
+        short ano = 2024;
+        int cep = 123456789; // Se começar com valor 0 talvez seja necessário usar outro tipo.
+        long cpf = 19876543210L; // long precisa terminar com L; Se começar com valor 0 talvez seja necessário usar outro tipo.
+        float PI = 3.14F; // float precisa terminar com F
     }
 }
